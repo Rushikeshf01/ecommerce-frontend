4ae6eff34406cb4c10012ad9ffa0ee4a8280f2fd
Preyansh <patelpreyansh3011@gmail.com>
2023-02-01 23:10:07 +0530

    basic toast message configure

---
+++ src/utils/toastMessages.ts
@@ -0,0 +1,18 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const ToastSuccessMessage = (msg: string) => {
+  toast.success(msg);
+};
+export const ToastDefaultMessage = (msg: string) => {
+  toast(msg);
+};
+export const ToastInfoMessage = (msg: string) => {
+  toast.info(msg);
+};
+export const ToastDangerMessage = (msg: string) => {
+  toast.error(msg);
+};
+export const ToastWarnMessage = (msg: string) => {
+  toast.warn(msg);
+};
