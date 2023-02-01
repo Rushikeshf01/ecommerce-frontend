ebc35b2c982d0065f0511539ed0964ec0433ea6e
Preyansh <patelpreyansh3011@gmail.com>
2023-02-01 23:17:08 +0530

    css configure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import App from "./App";
-import { store} from '@store/store'
+import { store } from "../store/store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import "./login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div className="login">Login</div>;
 };
 
 export default Login;
