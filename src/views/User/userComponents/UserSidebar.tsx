29c162a935cf73ee17f81f17b53fa0e3ee5755ea
Preyansh <patelpreyansh3011@gmail.com>
2023-02-18 22:20:14 +0530

    log out and delete account button added

--- src/views/User/userComponents/UserSidebar.tsx
+++ src/views/User/userComponents/UserSidebar.tsx
@@ -15,32 +15,32 @@ const UserSidebar = () => {
   const userSidebarLinks = [
     {
       icon: <AccountCircleOutlined />,
-      text: "My details",
+      text: "My Details",
       link: ApplicationConstant.USER_URL_PATH,
     },
     {
       icon: <ShoppingCartOutlined />,
-      text: "My cart",
+      text: "My Cart",
       link: ApplicationConstant.USER_CART_URL_PATH,
     },
     {
+      icon: <FavoriteBorderOutlined />,
+      text: "My Favorites",
+      link: ApplicationConstant.USER_FAVORITES_URL_PATH,
+    },
+    {
       icon: <RoomOutlined />,
-      text: "My saved addresses",
+      text: "My Addresses",
       link: ApplicationConstant.USER_ADDRESSES_URL_PATH,
     },
     {
       icon: <ShoppingBagOutlined />,
-      text: "My orders",
+      text: "My Orders",
       link: ApplicationConstant.USER_ORDERS_URL_PATH,
     },
     {
-      icon: <FavoriteBorderOutlined />,
-      text: "My favorites",
-      link: ApplicationConstant.USER_FAVORITES_URL_PATH,
-    },
-    {
       icon: <Edit />,
-      text: "My reviews",
+      text: "My Reviews",
       link: ApplicationConstant.USER_REVIEWS_URL_PATH,
     },
   ];
@@ -61,6 +61,18 @@ const UserSidebar = () => {
           <p>{item.text}</p>
         </Link>
       ))}
+      <div>
+        <button type="submit" className="user-sidebar-logout-button">
+          Log Out
+        </button>
+        <button
+          type="submit"
+          className="user-sidebar-deleteaccount-button not-allowed"
+          disabled
+        >
+          Delete Account
+        </button>
+      </div>
     </div>
   );
 };
