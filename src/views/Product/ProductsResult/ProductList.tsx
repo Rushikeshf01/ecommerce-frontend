43851358b0ce5423d1047af035e178bcc441efd2
Preyansh <patelpreyansh3011@gmail.com>
2023-03-08 23:20:29 +0530

    product list updated

--- src/views/Product/ProductsResult/ProductList.tsx
+++ src/views/Product/ProductsResult/ProductList.tsx
@@ -5,29 +5,39 @@ import { ApplicationConstant } from "../../../constant/applicationConstant";
 import { ProductVerticleCardProps } from "../../../types/authProps";
 import { capitalizeFirstLetter } from "../../../utils/jsFunctionsUtils";
 import ProductVerticalCard from "../../User/userComponents/UserCart/ProductVerticalCard";
-import ProductHorizontalView from "./ProductHorizontalView"
+import ProductHorizontalView from "./ProductHorizontalView";
 
 const breadCrumpsState = [
-    {
-        previousText: "Home",
-        previousLink: ApplicationConstant.HOME_URL_PATH,
-    },
-]
-const ProductList = (props: { productDetailList: ProductVerticleCardProps[]}) => {
-    const param = useParams()
-    const categoryName = param.category? param.category: ''
-    return (
-        <div className="items">
-            <AppBreadcrumps
-                previousData={breadCrumpsState}
-                currentData={categoryName.charAt(0).toUpperCase() + categoryName.slice(1)}
-                className="margin-top-5"
-            />
-            {props.productDetailList.map((item, index) => (
-                <ProductVerticalCard key={`category-productList-item-${index}`} cardDetails={item} isCartCard={false}/>
-            ))}
-        </div>
-    )
-}
+  {
+    previousText: "Home",
+    previousLink: ApplicationConstant.HOME_URL_PATH,
+  },
+];
+const ProductList = (props: {
+  productDetailList: ProductVerticleCardProps[];
+}) => {
+  const param = useParams();
+  const categoryName = param.category ? param.category : "";
+  return (
+    <div className="items">
+      <AppBreadcrumps
+        previousData={breadCrumpsState}
+        currentData={
+          categoryName.charAt(0).toUpperCase() + categoryName.slice(1)
+        }
+        className="margin-top-5"
+      />
+      {props.productDetailList.map((item, index) => (
+        <ProductVerticalCard
+          key={`category-productList-item-${index}`}
+          cardDetails={item}
+          isCartCard={false}
+          isCartIconShow
+          isFavoriteIconShow
+        />
+      ))}
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
